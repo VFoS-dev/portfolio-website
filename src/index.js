eacb514fee3ea0bc610c8efe3ea04c33b6d429a5
LongJonJon <46792496+LongJonJon@users.noreply.github.com>
2021-04-02 23:23:30 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import './css/index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { CustomRouter } from './components/CustomRouter';
-
+import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
 import { store } from './_helpers';
+import { CustomRouter } from './components/CustomRouter';
+
+import './css/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 render(
   <Provider store={store}>
