bbc47906c5db38c224f0c8a131861a14c27f7685
LongJonJon <46792496+LongJonJon@users.noreply.github.com>
2021-04-23 13:27:03 -0600

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import '../css/pages.css';
+import self from '../pictures/self.jpg';
 
 class About extends React.Component {
     render() {
         return (<div className="about">
-            This Website is still a work in progress
+            <center>
+                <img className='self' src={self} />
+            </center>
+            <div className='temp'>
+                <p>Hello my name is Jon Kido</p>
+                <p>was a peer mentor nicknamed 'The Wizard' because of my ability to debug almost everything thrown at me</p>
+                <p>field of studies: <strong>GIMM</strong> (Games, Interactive Media, and Mobile), <strong>MATH</strong> (Applied Mathematics), <strong>ITM</strong> (Information Technology Management)</p>
+            </div>
         </div>);
     }
 }
