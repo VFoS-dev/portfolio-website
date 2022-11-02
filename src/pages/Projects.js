8747c432ce4d23e3841baf9799b5fcbf3108e214
Jon Kido <46792496+VFoS-dev@users.noreply.github.com>
2022-11-02 00:16:10 -0600

    minesweeper header

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -13,7 +13,10 @@ class Projects extends React.Component {
             minesweeper: false,
             cells: [],
             nbombs: 0,
+            flags: 0,
+            gameStatus: 0,
             refresh: false,
+            rows: 10,
             container: -1,
             projects: [
                 { img: '/images/projects/portfolioSite.png', imgcss: { backgroundColor: '#303030' } },
@@ -34,6 +37,7 @@ class Projects extends React.Component {
         this.selfRef = React.createRef();
         this.handleMove = this.handleMove.bind(this)
         this.resize = this.resize.bind(this)
+        this.checkWin = this.checkWin.bind(this)
 
         window.addEventListener('resize', this.resize);
     }
@@ -71,19 +75,33 @@ class Projects extends React.Component {
     }
 
     mapTile({ name = '', description = '', img = "", imgcss = {} }) {
-        const { toMine } = this.state;
+        const { toMine, rows } = this.state;
         return (
             <div id='tile' style={{ backgroundImage: `url(${img})`, ...imgcss, ...(toMine ? { boxShadow: 'none', cursor: 'auto' } : {}) }} onMouseMove={this.handleMove} onMouseOut={(e) => this.handleMoveOut(e.target)}>
                 {toMine && <div className='toMinesweeper' onAnimationEnd={() => this.setState({
                     minesweeper: true,
-                    cells: [...new Array(10)].map(n => [...new Array(10)])
+                    cells: [...new Array(rows)].map(n => [...new Array(rows)])
                 })} />}
             </div>
         );
     }
 
+    async checkWin() {
+        const { cells } = this.state
+
+        var lost = false, win = 0
+        cells.forEach(_ => _.forEach(c => {
+            if (lost) return;
+            if (c.value < 0 && c.revealed) lost = true;
+            if (c.value >= 0 && c.revealed || c.value < 0 && !c.revealed) win++;
+        }))
+        win = win == cells.length * cells.length
+
+        if (win || lost) this.setState({ gameStatus: +win || -1 })
+    }
+
     createGame(index) {
-        const { cells } = this.state;
+        const { cells, projects } = this.state;
         let _c = [...new Array(cells.length)].map(n => [...new Array(cells.length)])
 
         for (var j = -1; j <= 1; j++)
@@ -105,7 +123,7 @@ class Projects extends React.Component {
         _c = _c.map((a, index) => {
             let x = index;
             return a.map((b, index) => {
-                if (b === -1) return { value: b, revealed: false, flagged: false }
+                if (b === -1) return { value: b, revealed: false, flagged: false, img: Math.floor(projects.length * Math.random()) }
                 let count = 0
                 for (var j = -1; j <= 1; j++)
                     for (var k = -1; k <= 1; k++) {
@@ -113,11 +131,11 @@ class Projects extends React.Component {
                         count += parseInt(_c[x + j][index + k]) || 0
                     }
 
-                return { value: -count || 0, revealed: false, flagged: false }
+                return { value: -count || 0, revealed: false, flagged: false, img: Math.floor(projects.length * Math.random()) }
             })
         })
 
-        this.setState({ cells: this.floodReveal(index, _c), nbombs: nbombs })
+        this.setState({ cells: this.floodReveal(index, _c), nbombs: nbombs, flags: 0 })
     }
 
     floodReveal(index, cells) {
@@ -156,22 +174,35 @@ class Projects extends React.Component {
 
     minesweep(id) {
         var index = id.split(' ').map(i => parseInt(i))
-        const { cells } = this.state
+        const { cells, gameStatus } = this.state
+        if (!!gameStatus) return;
         if (cells[index[0]][index[1]] === undefined) this.createGame(index)
-        else this.setState({ cells: this.floodReveal(index, cells) })
+        else {
+            this.setState({ cells: this.floodReveal(index, cells) })
+            this.checkWin()
+        }
+    }
+
+    changeState() {
+        const { minesweeper, cells, rows } = this.state;
+        if (minesweeper)
+            if (!cells[0][0]) this.setState({ toMine: false, minesweeper: false, gameStatus: 0 });
+            else this.setState({ cells: [...new Array(rows)].map(n => [...new Array(rows)]), nbombs: 0, flags: 0, time: 0, gameStatus: 0 })
+        else this.setState({ toMine: true })
     }
 
     flagCell(e) {
         e.preventDefault()
         const index = e.target.id.split(' ').map(i => parseInt(i))
-        let { cells } = this.state
+        let { flags, cells, gameStatus } = this.state
+        if (!!gameStatus) return;
         if (!cells[index[0]][index[1]]) return;
         cells[index[0]][index[1]].flagged = !cells[index[0]][index[1]].flagged;
-        this.setState({ cells: cells })
+        this.setState({ cells: cells, flags: flags + (!cells[index[0]][index[1]].flagged ? -1 : 1) })
     }
 
     render() {
-        const { minesweeper, cells, projects, refresh, container, nbombs } = this.state;
+        const { minesweeper, cells, projects, refresh, container, nbombs, flags, gameStatus } = this.state;
         if (!minesweeper && !this.selfRef.current) setTimeout(() => this.setState({ refresh: !refresh }), 0);
         if (container < 0 && !this.selfRef.current) setTimeout(() => this.resize(), 0);
         return (<div className="projects">
@@ -180,14 +211,16 @@ class Projects extends React.Component {
                 <div className="mineHeader">
                     <div className='numbs left'>
                         {[...new Array(3)].map((a, index) =>
-                            <div className={`numb n${Math.floor((!minesweeper ? projects.length : nbombs) / Math.pow(10, 2 - index)) % 10}`} />
+                            <div className={`numb n${Math.floor((!minesweeper ? projects.length : nbombs - flags) / Math.pow(10, 2 - index)) % 10}`} />
                         )}
                     </div>
-                    <div className='button' onClick={() => this.setState({ toMine: true })} />
+                    <center className='button-container' onClick={() => this.changeState()} >
+                        <div className={`button${!minesweeper ? "" : { 0: ' play', '-1': ' lose', 1: ' win' }[gameStatus]}`} />
+                    </center>
                     <div className='numbs right'>
-                        <div className='numb' />
-                        <div className='numb' />
-                        <div className='numb' />
+                        <div className='numb n0' />
+                        <div className='numb n0' />
+                        <div className='numb n0' />
                     </div>
                 </div>
                 <div className='mineContainer' ref={this.selfRef}>
@@ -202,7 +235,7 @@ class Projects extends React.Component {
                             return <div className='row'>
                                 {m.map((c, index) => {
                                     return <div id={`${x} ${index}`} className={`cell${!c ? ' in' : c.revealed ? ' revealed' : c.flagged ? ' flagged' : ''}`} style={{ width: `${100 / cells.length}%` }} onClick={(e) => this.minesweep(e.target.id)} onContextMenu={(e) => this.flagCell(e)}>
-                                        {!c ? "" : c.revealed ? !c.value ? '' : c.value : <div id={`${x} ${index}`} className={c.flagged ? 'flag' : 'a'} >{c.flagged}</div>}
+                                        {!c ? "" : c.revealed ? !c.value ? '' : c.value : <div id={`${x} ${index}`} style={{ backgroundImage: `url(${projects[c.img].img})` }} className={c.flagged ? 'flag' : ''} >{c.flagged}</div>}
                                     </div>
                                 })}
                             </div>
