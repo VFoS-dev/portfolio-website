08761a06e3b88e77b7b7f48f621ae76286bc5a76
Jon Kido <46792496+VFoS-dev@users.noreply.github.com>
2022-11-09 12:58:47 -0700

    resume temp

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -11,6 +11,9 @@ class Resume extends React.Component {
                 <div className='navpadding' />
                 <center className='demoSpacing'>
                     <h2>THIS PAGE IS UNDER CONSTRUCTION</h2>
+                    <br />
+                    <br />
+                    <br /><a href='/pdf/resume.pdf' target="_blank">CLICK HERE FOR RESUME (temporary solution)</a>
                 </center>
             </div>
         );
