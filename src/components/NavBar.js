7ac6a2842e4042ff835ecf3de759816c3bb7d423
Jon Kido <46792496+VFoS-dev@users.noreply.github.com>
2022-11-10 15:12:34 -0700

    navbar resizing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import { NavDropdown, Navbar, Nav, Form, Container, Button, Offcanvas } from 'react-bootstrap';
 import '../css/navbar.css';
+
 
 class NavBar extends Component {
     constructor(props) {
@@ -51,28 +51,41 @@ class NavBar extends Component {
         const f = document.getElementById('focused')
         const _scrollPercent = f ? (document.documentElement.clientHeight >= f.scrollHeight) ? 1 : (f.scrollHeight == 0) ? 0 : f.scrollTop / Math.min(f.scrollHeight - f.offsetHeight, f.scrollHeight) : 1;
 
-        return (
-            <Navbar className="navbar navbar-expand-lg fixed-top bg-transparent disable" bg="light" expand="lg" onToggle={() => this.setState({ nav: !nav })} expanded={nav}>
-                <div className='navShadow' />
-                <Navbar.Brand className="pointer enable z-2" id='intro' style={{ position: 'relative' }} onClick={(e) => this.changePage(e.target.id)} onMouseOut={() => this.setState({ animateLogo: false })} onMouseEnter={() => this.setState({ animateLogo: true })}>
-                    <div className='navImage' style={{ '--frame': `${Math.round((_scrollPercent) * 16)}` }} />
-                    <div className='disable' style={{ position: 'absolute', display: 'flex', top: 0, left: 'min(11vh, 11vw)', height: '30%', marginRight: '15px', marginTop: '1vh' }}>
-                        {[...new Array(secretLength)].map((c, index) => (<div key={index + "checkpoints"} className={`checkpoint${correct ? " complete" : checkpoints[index] ? " correct" : typeof checkpoints[index] == 'boolean' ? " wrong" : ''}`}><div /></div>))}
-                    </div>
-                </Navbar.Brand>
-                <Navbar.Toggle className='enable z-2' />
-                <Navbar.Collapse className='disable z-2'>
-                    <Nav className="mr-auto">
-                        <Nav.Link className={((ref === 'about') ? "active" : "") + " lato enable"} id='about' onClick={(e) => this.changePage(e.target.id)}>About</Nav.Link>
-                        <Nav.Link className={((ref === 'projects') ? "active" : "") + " lato enable"} id='projects' onClick={(e) => this.changePage(e.target.id)}>Projects</Nav.Link>
-                        <Nav.Link className={((ref === 'skills') ? "active" : "") + " lato enable"} id='skills' onClick={(e) => this.changePage(e.target.id)}>Skills</Nav.Link>
-                        <Nav.Link className={((ref === 'resume') ? "active" : "") + " lato enable"} id='resume' onClick={(e) => this.changePage(e.target.id)}>Resume</Nav.Link>
-                        <Nav.Link className={((ref === 'socials') ? "active" : "") + " lato enable"} id='socials' onClick={(e) => this.changePage(e.target.id)}>Socials</Nav.Link>
-                        {correct && <Nav.Link className={((ref === 'secret') ? "active" : "") + " lato enable"} id='secret' onClick={(e) => this.changePage(e.target.id)}>Secret</Nav.Link>}
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        );
+        return (<Navbar key='lg' bg="dark" expand='lg' className="navbar navbar-expand-lg fixed-top bg-transparent disable" expanded={nav} onToggle={() => this.setState({ nav: !nav })}>
+            <Navbar.Brand className="brand pointer enable z-2" id='intro' style={{ position: 'relative' }} onClick={(e) => this.changePage(e.target.id)} onMouseOut={() => this.setState({ animateLogo: false })} onMouseEnter={() => this.setState({ animateLogo: true })}>
+                <div className='navImage' style={{ '--frame': `${Math.round((_scrollPercent) * 16)}` }} />
+                <div className='disable checkpoint-container'>
+                    {[...new Array(secretLength)].map((c, index) => (<div key={index + "checkpoints"} className={`checkpoint${correct ? " complete" : checkpoints[index] ? " correct" : typeof checkpoints[index] == 'boolean' ? " wrong" : ''}`}><div /></div>))}
+                </div>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" className="enable hamButton" />
+            <div className='navShadow' />
+
+            <Container fluid className="enable z-2">
+                <Navbar.Offcanvas
+                    id="offcanvasNavbar-expand-lg"
+                    aria-labelledby="offcanvasNavbarLabel-expand-lg"
+                    placement="end"
+                    className='greyBG'
+                >
+                    <Offcanvas.Header>
+                        <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
+                            VFoS.dev
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="mr-auto">
+                            <Nav.Link className={((ref === 'about') ? "active" : "") + " lato enable"} id='about' onClick={(e) => this.changePage(e.target.id)}>About</Nav.Link>
+                            <Nav.Link className={((ref === 'projects') ? "active" : "") + " lato enable"} id='projects' onClick={(e) => this.changePage(e.target.id)}>Projects</Nav.Link>
+                            <Nav.Link className={((ref === 'skills') ? "active" : "") + " lato enable"} id='skills' onClick={(e) => this.changePage(e.target.id)}>Skills</Nav.Link>
+                            <Nav.Link className={((ref === 'resume') ? "active" : "") + " lato enable"} id='resume' onClick={(e) => this.changePage(e.target.id)}>Resume</Nav.Link>
+                            <Nav.Link className={((ref === 'socials') ? "active" : "") + " lato enable"} id='socials' onClick={(e) => this.changePage(e.target.id)}>Socials</Nav.Link>
+                            {correct && <Nav.Link className={((ref === 'secret') ? "active" : "") + " lato enable"} id='secret' onClick={(e) => this.changePage(e.target.id)}>Secret</Nav.Link>}
+                        </Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar>);
     }
 }
 
