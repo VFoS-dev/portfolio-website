147c0f0bb0a4b69c28e78a545ea74debf3caf196
Jon Kido <46792496+VFoS-dev@users.noreply.github.com>
2023-08-23 00:02:16 -0600

    socials page rework

--- src/_data/SocialsData.js
+++ src/_data/SocialsData.js
@@ -1,90 +1,56 @@
-export const socialsData = [
+
+
+
+
+
+export const social_Data = [
     {
         name: 'Coming Soon',
+        href: 'game-start',
         gif: '/images/socials/game/pineapple.gif',
-        startRot: Math.random() * 360,
-        styles: {
-            outerLine: 'white',
-            innerLine: 'white',
-            textBorder: 'black',
-            textColor: 'white'
-        }
+        ring: '/images/socials/svg/soon.svg',
+        shadow: '/images/socials/svg/shadow.svg',
+        upper: { fill: 'white', stroke: 'black' },
+        lower: { fill: 'white', stroke: 'black' },
     },
     {
-        id: '',
         name: 'Github',
         gif: '/images/socials/github.gif',
-        startRot: Math.random() * 360,
         href: 'https://github.com/VFoS-dev',
-        styles: {
-            foreignShadow: <foreignObject x="2" y="2" width="164" height="164" mask="url(#donut)">
-                <div className="rainbowGradient" />
-            </foreignObject>,
-            foreign: <foreignObject x="2" y="2" width="164" height="164" mask="url(#donut)">
-                <div className="rainbowGradient" />
-            </foreignObject>,
-            outerLine: 'white',
-            innerLine: 'white',
-            textBorder: 'black',
-            textColor: 'white'
-        }
+        ring: '/images/socials/svg/github.svg',
+        shadow: '/images/socials/svg/shadow-rainbow.svg',
+        upper: { fill: 'white', stroke: 'black' },
+        lower: { fill: 'white', stroke: 'black' },
     },
     {
-        id: 'pattern1',
         name: 'Linkedin',
         gif: '/images/socials/linkedin.gif',
-        startRot: Math.random() * 360,
         href: 'https://www.linkedin.com/in/jon-kido-vfos/',
-        styles: {
-            fill: <linearGradient
-                id="pattern1"
-                gradientTransform="rotate(30)">
-                <stop offset="0%" stopColor="rgba(255,0,0,0.5)" />
-                <stop offset="20%" stopColor="rgba(255,0,0,0.5)" />
-                <stop offset="95%" stopColor="rgba(0,0,255,0.5)" />
-                <stop offset="100%" stopColor="rgba(0,0,255,0.5)" />
-            </linearGradient>,
-            outerLine: '#4b61db',
-            innerLine: '#db4b4b',
-            text: {
-                upperBorder: '#700000',
-                upperColor: 'yellow',
-                lowerBorder: '#002d8f',
-                lowerColor: '#dce6fc',
-            }
-        }
+        ring: '/images/socials/svg/linkedin.svg',
+        shadow: '/images/socials/svg/shadow.svg',
+        upper: { fill: 'yellow', stroke: '#700000' },
+        lower: { fill: '#dce6fc', stroke: '#002d8f' },
     },
     {
-        id: 'pattern3',
         name: 'YouTube',
         gif: '/images/socials/youtube.gif',
-        startRot: Math.random() * 360,
         href: 'https://www.youtube.com/channel/UCbHIwUTtZwRiiPTyl_3ncLQ/',
-        styles: {
-            fill: <pattern id="pattern3" width="25" height="1" patternUnits="userSpaceOnUse" patternTransform="rotate(45 50 50)">
-                <rect fill='rgba(0, 86, 255, 0.5)' width='25px' height='10px' />
-                <line stroke='rgba(61, 127, 255, 0.5)' strokeWidth="25px" y2="10" />
-            </pattern>,
-            outerLine: '#3469d1',
-            innerLine: '#3469d1',
-            textBorder: '#002d8f',
-            textColor: '#dce6fc'
-        }
+        ring: '/images/socials/svg/youtube.svg',
+        shadow: '/images/socials/svg/shadow.svg',
+        upper: { fill: '#dce6fc', stroke: '#002d8f' },
+        lower: { fill: '#dce6fc', stroke: '#002d8f' },
     },
     {
-        id: '',
         name: 'LeetCode',
         gif: '/images/socials/leetcode.gif',
-        startRot: Math.random() * 360,
         href: 'https://leetcode.com/VFoS/',
-        styles: {
-            foreign: <foreignObject x="2" y="2" width="164" height="164" mask="url(#donut)">
-                <div className="gradient" />
-            </foreignObject>,
-            outerLine: 'rgba(255,100,100,0.5)',
-            innerLine: 'rgba(255,100,100,0.5)',
-            textBorder: '#700000',
-            textColor: 'yellow'
-        }
+        ring: '/images/socials/svg/leetcode.svg',
+        shadow: '/images/socials/svg/shadow.svg',
+        upper: { fill: 'yellow', stroke: '#700000' },
+        lower: { fill: 'yellow', stroke: '#700000' },
     },
 ]
+export const socialsData = (rots = []) => {
+    if (rots.length) return social_Data.map((d, i) => ({ ...d, startRot: rots[i] }))
+    return social_Data.map(d => ({ ...d, startRot: Math.random() * 360 }))
+}
--- src/pages/Socials.js
+++ src/pages/Socials.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { timeout } from '../utils';
-import { socialsData } from '../_data';
+import { socialsData, social_Data } from '../_data';
 
 import '../css/socials.css';
 
@@ -104,35 +104,32 @@ class Socials extends React.Component {
         this.setState({ path: path });
     }
 
-    openLink = (index) => window.open(socialsData[index].href, '_blank');
+    openLink(index) {
+        const { href } = social_Data[index];
+        switch (href) {
+            case 'game-start': return console.log('create game!');
+            default: return window.open(href, '_blank');
+        }
+    }
 
-    createOptions(op, index) {
-        return <div className='grow' key={'options' + index}>
-            <div className='options'>
-                <div className='svg'>
-                    <svg className='shadow' viewBox="0 0 168 168" filter='blur(2.5em)'>
-                        <circle mask="url(#donut)" fill='rgba(10,10,10,1)' r="82" cx="84" cy="84" />
-                        {op.styles.foreignShadow}
-                    </svg>
-                    <svg className='rot' viewBox="0 0 168 168" style={{ '--rot': `${op.startRot}deg` }}>
-                        <defs>{op.styles.fill}</defs>
-                        {op.styles.foreign}
-                        <circle id="visual" mask="url(#donut)" fill={`url(#${op.id})`} stroke={op.styles.outerLine} strokeWidth="1.4" r="82" cx="84" cy="84" />
-                        <circle fill='none' stroke={op.styles.innerLine} strokeWidth="1.4" transform="matrix(1 0 0 1 28 28)" r="55.9095409898556" cx="55.9095409898556" cy="55.9095409898556" />
-                        <path id="upper" fill="none" transform="translate(19.7999976778584, 84) scale(1,-1)" d="M0 -1.26218e-29C-3.33663e-14 22.9365 12.2365 44.1306 32.1 55.5988C51.9636 67.0671 76.4365 67.0671 96.3 55.5988C116.164 44.1306 128.4 22.9365 128.4 1.09118e-13" />
-                        <path id="lower" fill="none" transform="matrix(-0.999980871067792 0.00618381781983816 -0.00618381781983816 -0.999980871067792 148.19877424465 83.6029988816068)  scale(1,-1)" d="M0 1.77501e-13C1.05879e-13 35.4567 28.7433 64.2 64.2 64.2C99.6567 64.2 128.4 35.4567 128.4 2.29597e-13" />
-                        <mask id='donut'>
-                            <rect fill='white' width='168px' height='168px' />
-                            <circle fill='black' transform="matrix(1 0 0 1 28 28)" r="55.9095409898556" cx="55.9095409898556" cy="55.9095409898556" />
-                        </mask>
-                        <text className='fnFont' alignmentBaseline="top">
-                            <textPath href="#upper" fill={op.styles.textColor || op.styles.text.upperColor} stroke={op.styles.textBorder || op.styles.text.upperBorder}>{op.name}</textPath>
-                            <textPath href="#lower" fill={op.styles.textColor || op.styles.text.lowerColor} stroke={op.styles.textBorder || op.styles.text.lowerBorder}>{op.name}</textPath>
+    createOptions({ name, gif, href, ring, shadow, upper, lower }, index) {
+        return <div key={`option-${name}`} className='option' style={{ backgroundImage: `url(${shadow})` }}>
+            <div className='option-group'>
+                <div className='circle'>
+                    <svg className='text' viewBox="0 0 168 168" xmlns="http://www.w3.org/2000/svg">
+                        <path id="upper" fill="none" transform="translate(19.7999976778584, 84) scale(1,-1)"
+                            d="M0 -1.26218e-29C-3.33663e-14 22.9365 12.2365 44.1306 32.1 55.5988C51.9636 67.0671 76.4365 67.0671 96.3 55.5988C116.164 44.1306 128.4 22.9365 128.4 1.09118e-13" />
+                        <path id="lower" fill="none"
+                            transform="matrix(-0.999980871067792 0.00618381781983816 -0.00618381781983816 -0.999980871067792 148.19877424465 83.6029988816068)  scale(1,-1)"
+                            d="M0 1.77501e-13C1.05879e-13 35.4567 28.7433 64.2 64.2 64.2C99.6567 64.2 128.4 35.4567 128.4 2.29597e-13" />
+                        <text className="fnFont" alignmentBaseline="top">
+                            <textPath href="#upper" {...upper}>{name}</textPath>
+                            <textPath href="#lower" {...lower}>{name}</textPath>
                         </text>
                     </svg>
+                    <img className='decor' src={ring} />
                 </div>
-                {op.href && <div className='hoverEvent' id={index} onClick={e => this.openLink(e.target.id, true)} />}
-                {op.gif && <img className='gif' src={op.gif} alt='' />}
+                <img className='gif' src={gif} id={index} onClick={(e) => this.openLink(e.target.id)} />
             </div>
         </div>
     }
@@ -149,7 +146,7 @@ class Socials extends React.Component {
             <div className="socials">
                 <div className='navpadding' />
                 <div className='links'>
-                    {socialsData.map((a, index) => this.createOptions(a, index))}
+                    {socialsData([]).map((a, index) => this.createOptions(a, index))}
                 </div>
             </div>
         </Fragment>);
