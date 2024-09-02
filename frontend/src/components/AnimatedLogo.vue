8d4483341b21b1f43e8b893c1d60638319a114ad
Jon Kido <46792496+VFoS-dev@users.noreply.github.com>
2024-09-02 17:35:56 -0600

    navbar start

---
+++ frontend/src/components/AnimatedLogo.vue
@@ -0,0 +1,44 @@
+<template>
+    <RouterLink to="/" :style="{ '--frame': frame }"> </RouterLink>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    scrollPercent: { type: Number, default: 0 },
+    hasScroll: { type: Boolean, default: false },
+})
+
+const frame = computed(() => {
+    const { scrollPercent, hasScroll } = props;
+    if (!hasScroll) return;
+
+    return Math.round(scrollPercent * 16)
+})
+</script>
+
+<style lang="less" scoped>
+@keyframes iconAni {
+    to {
+        background-position-x: 0%;
+    }
+
+    from {
+        background-position-x: 100%;
+    }
+}
+
+a {
+    aspect-ratio: 1/1;
+    background-image: url(/images/nav/logosprite.webp);
+    background-position-x: calc(var(--frame, 16) / 16 * 100%);
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: clamp(90px, 17vh, 121px);
+
+    &:hover {
+        animation: iconAni 1s steps(16) 500ms infinite alternate-reverse;
+    }
+}
+</style>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav>
+        <AnimatedLogo :hasScroll="false" :scrollPercent=".7" />
+        <div>
+
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import AnimatedLogo from "@/components/AnimatedLogo.vue";
+</script>
+
+<style lang="less" scoped>
+nav {
+    
+    position: absolute;
+    display: flex;
+    flex-direction: row;
+    width: 100dvw;
+    z-index: 12;
+    top: 0;
+    left: 0;
+    padding: 10px 15px;
+
+    &::before {
+        content: "";
+        pointer-events: none;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        z-index: -1;
+        background: -webkit-linear-gradient(rgba(1, 1, 1, 0.9), transparent);
+        height: clamp(100px, 18vh, 120px);
+    }
+}
+</style>
