254b7f4a38ad17e460d8eb999fbbb42ad2f10383
Jon Kido <46792496+VFoS-dev@users.noreply.github.com>
2025-12-22 22:04:38 -0700

    s

--- frontend/src/applications/Tetris/Board/Board.vue
+++ frontend/src/applications/Tetris/Board/Board.vue
@@ -29,6 +29,11 @@ const props = defineProps({
 </script>
 
 <style lang="less" scoped>
+@window-bg: #ece9d8;
+@inset-depth: 4px;
+@highlight: #ffffff;
+@shadow: #808080;
+
 .game {
   display: flex;
   flex-direction: row;
@@ -40,12 +45,39 @@ const props = defineProps({
   --color: green;
   --width: 10;
   --height: 24;
-  background-color: gray;
+  --grid-dark: #1a1a1a;
+  --grid-light: #2a2a2a;
+  
+  background-color: #000000;
+  background-image: 
+    repeating-linear-gradient(
+      0deg,
+      transparent,
+      transparent calc(100% / var(--height) - 1px),
+      var(--grid-light) calc(100% / var(--height) - 1px),
+      var(--grid-light) calc(100% / var(--height))
+    ),
+    repeating-linear-gradient(
+      90deg,
+      transparent,
+      transparent calc(100% / var(--width) - 1px),
+      var(--grid-light) calc(100% / var(--width) - 1px),
+      var(--grid-light) calc(100% / var(--width))
+    );
+  
   display: grid;
   grid-template-columns: repeat(var(--width, 1), 1fr);
   grid-template-rows: repeat(var(--height, 1), 1fr);
   height: calc(var(--size, 30px) * var(--height, 1));
   width: calc(var(--size, 30px) * var(--width, 1));
+  
+  border: @inset-depth inset @highlight;
+  border-top-color: @shadow;
+  border-left-color: @shadow;
+  box-shadow: 
+    inset 1px 1px 0 0 @shadow,
+    inset -1px -1px 0 0 @highlight;
+  padding: 2px;
 }
 </style>
 
--- frontend/src/applications/Tetris/Board/DisplayUI.vue
+++ frontend/src/applications/Tetris/Board/DisplayUI.vue
@@ -5,11 +5,21 @@
 </template>
 
 <style scoped lang="less">
+@inset-depth: 4px;
+@highlight: #ffffff;
+@shadow: #808080;
+
 .ui {
   width: calc(var(--size) * 5);
   padding: calc(var(--size) / 2) 0;
   min-height: calc(var(--size) * 3);
-  background-color: black;
+  background-color: #000000;
+  border: @inset-depth inset @highlight;
+  border-top-color: @shadow;
+  border-left-color: @shadow;
+  box-shadow: 
+    inset 1px 1px 0 0 @shadow,
+    inset -1px -1px 0 0 @highlight;
   display: flex;
   flex-direction: column;
   gap: calc(var(--size) / 2);
